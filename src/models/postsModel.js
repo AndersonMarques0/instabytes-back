63f2c4a7426e201ce0ac1a387a29558613a9cca5
AndersonMarques0 <119546595+AndersonMarques0@users.noreply.github.com>
2024-11-27 18:02:27 -0300

    uptade

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,30 +1,28 @@
 import 'dotenv/config';
-// Importa a função para conectar ao banco de dados MongoDB
 import { ObjectId } from "mongodb";
-import conectarAoBanco from "../config/dbConfig.js"; 
+import conectarAoBanco from "../config/dbConfig.js"
+// Conecta ao banco de dados utilizando a string de conexão fornecida como variável de ambiente
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-// Conecta ao banco de dados MongoDB usando a string de conexão do ambiente
-const conexao = await conectarAoBanco(process.env.STRING_CONEXAO); 
-
-// Função assíncrona para obter todos os posts da coleção "posts"
+// Função assíncrona para buscar todos os posts do banco de dados
 export async function getTodosPosts() {
-    // Obtém o banco de dados "imersao-instabytes"
+    // Seleciona o banco de dados "imersao-instabytes"
     const db = conexao.db("imersao-instabytes");
-    // Obtém a coleção "posts" dentro do banco de dados
+    // Seleciona a coleção "posts" dentro do banco de dados
     const colecao = db.collection("posts");
-    // Busca todos os documentos da coleção e retorna como um array
+    // Retorna um array com todos os documentos da coleção
     return colecao.find().toArray();
 }
 
 export async function criarPost(novoPost) {
-  const db = conexao.db("imersao-instabytes");
-  const colecao = db.collection("posts");
-  return colecao.insertOne(novoPost);
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost);
 }
 
 export async function atualizarPost(id, novoPost) {
-  const db = conexao.db("imersao-instabytes");
-  const colecao = db.collection("posts")
-  const objID = ObjectId.createFromHexString(id)
-  return colecao.updateOne({_id: new ObjectId(objID)}, {$set:novoPost});
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    const objID = ObjectId.createFromHexString(id);
+    return colecao.updateOne({_id: new ObjectId(objID)}, {$set:novoPost});
 }
